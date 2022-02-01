fd6e178170e7a7f9ee2d1e904c91fe2fdea25e89
ahsan-tikito <ahsan@tikito.app>
2022-02-01 17:19:43 +0500

    debug false and deleted extra files

--- Fingen/settings.py
+++ Fingen/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y13#v6w+v$58smcqf5$@1&ybgmppx$(raj58=g0@4i$jjxhph$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
